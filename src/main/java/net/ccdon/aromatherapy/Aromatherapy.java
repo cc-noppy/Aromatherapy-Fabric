026abd91efd8e0f4a26f9ba6d762f71af454af56
cc-noppy <francislorenzrosas@gmail.com>
2022-06-17 01:02:43 -0700

    Initial commit

---
+++ src/main/java/net/ccdon/aromatherapy/Aromatherapy.java
@@ -0,0 +1,14 @@
+package net.ccdon.aromatherapy;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Aromatherapy implements ModInitializer {
+	public static final String MOD_ID = "aromatherapy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/ccdon/aromatherapy/AromatherapyClient.java
@@ -0,0 +1,10 @@
+package net.ccdon.aromatherapy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AromatherapyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
